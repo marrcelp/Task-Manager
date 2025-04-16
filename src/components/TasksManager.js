e902ad6517349815c363e2b6a3d9a18ab0e90a06
marrcelp <marcel.piaszczyk@gmail.com>
2025-04-16 21:50:27 +0200

    created project basic structure with timer, add taskProvider

--- src/components/TasksManager.js
+++ src/components/TasksManager.js
@@ -1,17 +1,132 @@
 import React from 'react';
+import { sendTask, fetchTask } from './tasksProvider';
+const url = 'http://localhost:3005/data';
 
 class TasksManager extends React.Component {
     state = {
         tasks: [],
+        taskName: '',
+        time: 0,
+        isRunning: false,
+        isDone: false,
+        isRemoved: false
+    }
+
+    componentDidMount(){
+        fetchTask(url)
+            .then(tasks => {
+                this.setState({
+                    tasks: tasks
+                })
+            })
+            .catch(err => {
+                console.error('Blad przy pobieraniu danych:', err);
+            })
     }
 
     onClick = () => {
         const { tasks } = this.state;
-        console.log( tasks)
+        console.log(tasks)
     }
 
+    addTask = (task) => {
+        this.setState({
+            tasks: [...this.state.tasks, task]
+        })
+    }
+
+    submitHandler = (e) => {
+        e.preventDefault();
+        
+        const { taskName, time } = this.state;
+        if(taskName){
+            const newTask = {
+                name: taskName,
+                time: parseInt(time),
+                isRunning: false,
+                isDone: false,
+                isRemoved: false
+            }
+
+            sendTask(url, newTask)
+                .then((savedTask) => {
+                    this.setState({
+                        tasks: [...this.state.tasks, savedTask],
+                        taskName: ''
+                    }, () => console.log(this.state.tasks))
+                })
+                .catch((err) =>{
+                    alert('Blad przy zapisie zadania. Spróbuj ponownie');
+                    console.error('Blad przy zapisie taska:', err);
+                })
+
+        } else {
+            alert ('Pole Task name nie moze być puste!');
+        }
+
+    }
+
+    inputChange = e => {
+        const {name, value} = e.target;
+        this.setState({
+            [name]: value
+        })
+    }
+
+    incrementTime(id){
+        const timer = setInterval(() => {
+            this.setState(state => {
+                const newTasks = state.tasks.map(task => {
+                    if(task.id === id){
+                        return {...task, time: task.time +1 }
+                    }
+
+                    return task;
+                });
+
+                return {
+                    tasks: newTasks,
+                }
+            })
+        }, 1000)
+    }
+
+    formatTime = (totalSeconds) => {
+        const hours = String(Math.floor(totalSeconds / 3600)).padStart(2, '0');
+        const minutes = String(Math.floor((totalSeconds % 3600) / 60)).padStart(2, '0');
+        const seconds = String(totalSeconds % 60).padStart(2, '0');
+    
+        return `${hours}:${minutes}:${seconds}`;
+    }
+    
+
     render() {
-        return <h1 onClick={ this.onClick }>TasksManager</h1>
+        const { taskName, tasks } = this.state;
+        return (
+            <>
+            <section>
+                <h1 onClick={ this.onClick }>TasksManager</h1>
+                <form className='task__form' onSubmit={this.submitHandler}>
+                    <input name='taskName' value={ taskName } onChange={ this.inputChange } className='task__name' type='text' placeholder='Task name'></input>
+                    <button className='task__button' type='submit'>Add task</button>
+                </form>
+            </section>
+            <section>
+                <ul>
+                    {tasks.map((task)=> {
+                        return(
+                        <li>
+                            <header>{task.name}, {this.formatTime(task.time)}</header>
+                            <button onClick={() => this.incrementTime(task.id)}>start/stop</button>
+                            <button>zakończone</button>
+                            <button disabled="true">usuń</button>
+                        </li>
+                        )
+                    })}
+                </ul>
+            </section>
+            </>
+        )
     }
 }
 
---
+++ src/components/tasksProvider.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+export function sendTask(url, data) {
+    const options = {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: { 'Content-Type': 'application/json' },
+    }
+
+    const promise = fetch(url, options)
+
+    return promise
+        .then((resp) => {
+            if (resp.ok){
+                return resp.json();
+            }
+            if (resp.status === 400){
+                return Promise.reject('Bad query to API')
+            }
+            return Promise.reject(resp);
+        })
+
+        .then((task) => {
+            return task
+        })
+
+        .catch(err => console.error(err))
+        .finally(() => console.log('Zakonczono odpytywanie do API'));
+
+}
+
+export function fetchTask(url){
+
+    const promise = fetch(url);
+
+    return promise
+        .then((resp) => {
+            if (resp.ok){
+                return resp.json();
+            }
+            if (resp.status === 400){
+                return Promise.reject('Bad query to API')
+            }
+            return Promise.reject(resp);
+        })
+
+        .then((tasks) => {
+            return tasks
+        })
+
+        .catch(err => console.error(err))
+        .finally(() => console.log('Zakonczono odpytywanie do API'));
+
+}
+
+
