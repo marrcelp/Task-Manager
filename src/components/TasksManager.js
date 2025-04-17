180166bc65b4510c8c1375b1ca9e492778c178bd
marrcelp <marcel.piaszczyk@gmail.com>
2025-04-17 17:49:02 +0200

    add classNames

--- src/components/TasksManager.js
+++ src/components/TasksManager.js
@@ -170,24 +170,24 @@ class TasksManager extends React.Component {
         const { taskName, tasks } = this.state;
         return (
             <>
-            <section>
+            <section className='task__top'>
                 <h1 onClick={ this.onClick }>TasksManager</h1>
                 <form className='task__form' onSubmit={this.submitHandler}>
                     <input name='taskName' value={ taskName } onChange={ this.inputChange } className='task__name' type='text' placeholder='Task name'></input>
                     <button className='task__button' type='submit'>Add task</button>
                 </form>
             </section>
-            <section>
-                <ul>
+            <section className='task__bottom'>
+                <ul className='task__list'>
                     {tasks
                     .sort((a, b) => a.isDone - b.isDone)
                     .map((task)=> {
                         return(
-                        <li style={{ display: task.isRemoved ? 'none' : 'block' }}>
-                            <header>{task.name}, {this.formatTime(task.time)}</header>
-                            <button onClick={() => task.isRunning? this.stopTime(task.id) : this.incrementTime(task.id)}>{task.isRunning ? 'Stop' : 'Start'}</button>
-                            <button onClick={() => this.finishTask(task.id)}>zakończone</button>
-                            <button onClick={() => this.removeTask(task.id)} disabled={!task.isDone}>usuń</button>
+                        <li className='task__element' style={{ display: task.isRemoved ? 'none' : 'block' }}>
+                            <header className='task__header'>{task.name}, {this.formatTime(task.time)}</header>
+                            <button className='task__btn' onClick={() => task.isRunning? this.stopTime(task.id) : this.incrementTime(task.id)}>{task.isRunning ? 'Stop' : 'Start'}</button>
+                            <button className='task__btn' onClick={() => this.finishTask(task.id)}>zakończone</button>
+                            <button className='task__btn' onClick={() => this.removeTask(task.id)} disabled={!task.isDone}>usuń</button>
                         </li>
                         )
                     })}
